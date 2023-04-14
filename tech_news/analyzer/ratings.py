a77d617fcf8c94f11edc76438e9cc4bb9e107474
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-14 18:38:05 -0300

    feat: Create top_5_categories function
    Create function to get top five categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+
+    if len(news) == 0:
+        return []
+
+    categories = dict()
+
+    for new in news:
+        if new["category"] in categories:
+            categories[new["category"]] += 1
+        else:
+            categories[new["category"]] = 1
+
+    top_5 = list(categories.items())
+    top_5.sort(key=lambda x: (x[1]*-1, x[0]))
+
+    return [category[0] for category in top_5[:5]]
