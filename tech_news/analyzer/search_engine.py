ca759b6a606aa4e293db3526d8abf9216a9648e2
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-14 15:42:54 -0300

    feat: Create search_by_date and validate_date functions
    Create function to get news by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,13 @@
 from tech_news.database import search_news
+import datetime
+
+
+def validate_date(date):
+    try:
+        year, month, day = map(int, date.split('-'))
+        datetime.datetime(year, month, day)
+    except ValueError:
+        raise ValueError('Data inválida')
 
 
 # Requisito 7
@@ -13,7 +22,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+
+    new_format_date = '/'.join(date.split('-')[::-1])
+
+    news = search_news({"timestamp": {"$eq": new_format_date}})
+
+    if len(news) > 0:
+        return [(new["title"], new["url"]) for new in news]
+
+    return news
 
 
 # Requisito 9
